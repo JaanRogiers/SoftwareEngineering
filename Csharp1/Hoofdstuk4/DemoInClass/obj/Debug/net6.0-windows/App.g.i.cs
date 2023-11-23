c06aad8b4603e7f95c0a25c087ebd2a79ae45a69
JaanRogiers <jaan.rogiers@hotmail.com>
2023-11-23 19:04:56 +0100

    Fix gitignores

--- Csharp1/Hoofdstuk4/DemoInClass/obj/Debug/net6.0-windows/App.g.i.cs
+++ Csharp1/Hoofdstuk4/DemoInClass/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace DemoInClass {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace DemoInClass {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public static void Main() {
             DemoInClass.App app = new DemoInClass.App();
             app.InitializeComponent();
---
+++ Csharp1/Hoofdstuk4/Oefening4.1/README.md
@@ -0,0 +1,2 @@
+# Opgave 4.1
+Breid het rechtboekprogramma van dit hoofdstuk uit naar een programma dat het volume van een blok berekent als de afmetingen van de drie zijden gegeven zijn.
